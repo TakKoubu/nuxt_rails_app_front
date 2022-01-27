9de49f2bc915a7d73c2dc7246fc591356b65bc5a
TakKokubu <kokubu.tk@gmail.com>
2022-01-27 17:28:08 +0900

    layoutの作成

---
+++ pages/user/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="admin-auth-page">
+    hoggehoge
+    <div class="auth-container">
+      <form @submit.prevent="onSubmit">
+        <AppControlInput type="email" v-model="email">E-Mail</AppControlInput>
+        <AppControlInput type="password" v-model="password">Password</AppControlInput>
+        <AppButton type="submit">新規登録</AppButton>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserPage",
+  layout: "default",
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$store.dispatch("authenticateUser", {
+        email: this.email,
+        password: this.password
+      })
+      .then(() => {
+        this.$router.push('/');
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.admin-auth-page {
+  padding: 20px;
+}
+
+.auth-container {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 2px #ccc;
+  width: 300px;
+  margin: auto;
+  padding: 10px;
+  box-sizing: border-box;
+}
+</style>
