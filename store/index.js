a0377e99864927dfcbb683361f44a27e57da4d00
TakKokubu <kokubu.tk@gmail.com>
2022-01-29 07:54:13 +0900

    init authのコメントアウト

--- store/index.js
+++ store/index.js
@@ -64,35 +64,35 @@ const createStore = () => {
           // })
           .catch(e => console.log(e));
       },
-      initAuth(vuexContext, req) {
-        let token;
-        let expirationDate;
-        if (req) {
-          if (!req.headers.cookie) {
-            return;
-          }
-          const jwtCookie = req.headers.cookie
-            .split(";")
-            .find(c => c.trim().startsWith("jwt="));
-          if (!jwtCookie) {
-            return;
-          }
-          token = jwtCookie.split("=")[1];
-          expirationDate = req.headers.cookie
-            .split(";")
-            .find(c => c.trim().startsWith("expirationDate="))
-            .split("=")[1];
-        } else if (process.client) {
-          token = localStorage.getItem("token");
-          expirationDate = localStorage.getItem("tokenExpiration");
-        }
-        if (new Date().getTime() > +expirationDate || !token) {
-          console.log("No token or invalid token");
-          vuexContext.dispatch("logout");
-          return;
-        }
-        vuexContext.commit("setToken", token);
-      },
+      // initAuth(vuexContext, req) {
+      //   let token;
+      //   let expirationDate;
+      //   if (req) {
+      //     if (!req.headers.cookie) {
+      //       return;
+      //     }
+      //     const jwtCookie = req.headers.cookie
+      //       .split(";")
+      //       .find(c => c.trim().startsWith("jwt="));
+      //     if (!jwtCookie) {
+      //       return;
+      //     }
+      //     token = jwtCookie.split("=")[1];
+      //     expirationDate = req.headers.cookie
+      //       .split(";")
+      //       .find(c => c.trim().startsWith("expirationDate="))
+      //       .split("=")[1];
+      //   } else if (process.client) {
+      //     token = localStorage.getItem("token");
+      //     expirationDate = localStorage.getItem("tokenExpiration");
+      //   }
+      //   if (new Date().getTime() > +expirationDate || !token) {
+      //     console.log("No token or invalid token");
+      //     vuexContext.dispatch("logout");
+      //     return;
+      //   }
+      //   vuexContext.commit("setToken", token);
+      // },
       logout(vuexContext) {
         vuexContext.commit("clearToken");
         Cookie.remove("jwt");
