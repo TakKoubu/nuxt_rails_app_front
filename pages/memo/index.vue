6e88058a88b697f3524b67be4c42d56bea9554dc
TakKokubu <kokubu.tk@gmail.com>
2022-01-29 16:24:04 +0900

    logout機能の追加

--- pages/memo/index.vue
+++ pages/memo/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="admin-page">
     <section class="new-post">
-      <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton>
+      <!-- <AppButton @click="$router.push('/admin/new-post')">Create Post</AppButton> -->
       <AppButton style="margin-left: 10px" @click="onLogout">Logout</AppButton>
     </section>
     <section class="existing-posts">
-      <h1>Existing Memos</h1>
+      <h1>Existing Posts</h1>
     </section>
   </div>
 </template>
@@ -18,9 +18,25 @@ export default {
   },
   methods: {
     onLogout() {
-      this.$store.dispatch("logout");
-      this.$router.push("/user");
+      this.$auth.logout();
+      this.$router.push("/login");
     }
   }
 };
 </script>
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #ccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+</style>
